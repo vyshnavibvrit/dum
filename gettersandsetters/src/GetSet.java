9edbc24aeaf89986b755e8fc623f627f2ab576d8
admin <admin@cselab9-47>
2020-03-12 15:20:09 +0530

    commit msg

---
+++ gettersandsetters/src/GetSet.java
@@ -0,0 +1,32 @@
+
+public class GetSet {
+	String CustomerName;
+	String AadharNo;
+	int MobileNo;
+	String CustAddr;
+	public String getCustomerName() {
+		return CustomerName;
+	}
+	public void setCustomerName(String customerName) {
+		CustomerName = customerName;
+	}
+	public String getAadharNo() {
+		return AadharNo;
+	}
+	public void setAadharNo(String aadharNo) {
+		AadharNo = aadharNo;
+	}
+	public int getMobileNo() {
+		return MobileNo;
+	}
+	public void setMobileNo(int mobileNo) {
+		MobileNo = mobileNo;
+	}
+	public String getCustAddr() {
+		return CustAddr;
+	}
+	public void setCustAddr(String custAddr) {
+		CustAddr = custAddr;
+	}
+	
+}
